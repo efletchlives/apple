eedbc63e14ca0617253c5642b41ae3f5e37ab1f9
Rafael Valladares <rav84@pitt.edu>
2025-10-31 16:10:01 -0400

    Refactor setup and game logic with timer integration
    Refactor setup and actionChoice functions to include timer parameter. Update display initialization and game state transitions.

--- Defuse_It_Top/Defuse_It_Top.ino
+++ Defuse_It_Top/Defuse_It_Top.ino
@@ -1,3 +1,11 @@
+#include <Adafruit_SSD1306.h>
+#include <Wire.h>
+#include "ADS1X15.h"
+
+#define OLED_RESET 4
+Adafruit_SSD1306 Display1(OLED_RESET);
+Adafruit_SSD1306 Display2(OLED_RESET);
+ADS1115 ADS(0x48);
 
 //ENUM For Each Different State
 enum State
@@ -9,20 +17,38 @@ enum State
 };
 
 //Global Variable Initialization
+double timer = 10000;
 int lives;
 int score;
 const int buttonPin = 2;
 State currentState = s0;
 bool actionSuccess;
 
-void setupMain() {
+void setup() {
   pinMode(buttonPin, INPUT);
   Serial.begin(9600);
+
+  //DISPLAY SETUP
+  Display1.begin(SSD1306_SWITCHCAPVCC, 0x3C);
+  Display1.clearDisplay();
+  Display1.display();
+
+  Display2.begin(SSD1306_SWITCHCAPVCC, 0x3D);
+  Display2.clearDisplay();
+  Display2.display();
+
+  //WIRE SETUP
+  Wire.begin();
+  ADS.begin();
+  ADS.setGain(0);
+  ADS.setDataRate(7);
+  ADS.setMode(1);
+  randomSeed(analogRead(0));
 
 
 }
 
-bool actionChoice()
+void actionChoice(int timer)
 {
   //1: keypad
   //2: Wires
@@ -33,26 +59,54 @@ bool actionChoice()
 
   if(choice == 1)
   {
-    keypadSuccess = keypadLoop();
+    actionSuccess = keypadLoop(timer);
   }
   else if(choice == 2)
   {
-    wiresLoop();
+    actionSuccess = wiresLoop(timer);
   }
   else if(choice == 3)
   {
     //mic loop
   }
-
-  
-
-
-
 }
 
-void GameLoop() 
-{
 
+void startScreen()
+{
+  Display1.clearDisplay();
+  Display1.setCursor(0,0);
+  Display1.println("DEFUSE-IT----PRESS BUTTON TO START");
+  Display1.display();
+  delay(2000);
+}
+
+void winScreen()
+{
+  Display1.clearDisplay();
+  Display1.setCursor(0,0);
+  Display1.println("BOMB DEFUSED");
+  Display1.setCursor(0,0);
+  Display1.println(score);
+  Display1.display();
+  delay(2000);
+}
+
+void failScreen()
+{
+  Display1.clearDisplay();
+  Display1.setCursor(0,0);
+  Display1.println("FAILED TO DEFUSE BOMB");
+  Display1.setCursor(0,0);
+  Display1.println(score);
+  Display1.display();
+  delay(2000);
+}
+
+
+void loop() 
+{
+   
   // ----------FSM BEHAVIOR----------
   switch(currentState)
   {
@@ -63,18 +117,19 @@ void GameLoop()
       break;
     case s1:  //GAME LOOP STATE
       //call gameloop
-      actionChoice();
-      
-      
+      actionChoice(timer);
+    
       break;
     case s2:  //LOSE STATE
-      //call wire loop
-      
+      failScreen();
       break;
     case s3:  //WIN STATE
-      //call mic loop
+      winScreen();
       break;
   }
+
+  timer -= 71.43;
+  nextState();
 
 }
 
@@ -82,7 +137,7 @@ void GameLoop()
 void nextState()
 {
   //stays in state 0 until button is pressed
-  if(currentState == s0)
+  if(currentState == s0) //START UP STATE
   {
     while(digitalRead(buttonPin) == LOW)
     {
@@ -91,14 +146,40 @@ void nextState()
     currentState = s1;
   }
 
-  else if(currentState == s1)
+  else if(currentState == s1) //GAME LOOP STATE
   {
+    //if there are zero lives left, the game is lost
     if(lives == 0)
     {
+      currentState = s2;
+    }
 
+    //if the action is successfull but the score is less than 99 then we stay in the game loop
+    else if((actionSuccess == true) && (score < 99))
+    {
+      currentState = s1;
+    }
+
+    //if the last action was successfull and the score is 99 then the game is won
+    else if((actionSuccess == true) && (score == 99))
+    {
+      currentState = s3;
     }
   }
+
+  else if(currentState == s2) //GAME LOST
+  {
+    currentState = s0;
+  }
+
+  else if(currentState == s3) //GAME WON
+  {
+    currentState = s0;
+  }
 }
+
+
+
 
 
 
