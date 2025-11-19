ba73f920d014b279881f126a351a5a16d3a2d586
Arian-Ari <ATA57@PITT.EDU>
2025-11-19 01:11:59 -0500

    Update combo_mic.ino

--- Defuse_It_Top/combo_mic.ino
+++ Defuse_It_Top/combo_mic.ino
@@ -1,5 +1,8 @@
 // HW-484 Sound Sensor Test for ESP32-S3
-
+#include <Wire.h>
+#include "ADS1X15.h"
+#include "Voltage_Value.h"
+#include <Adafruit_SSD1306.h>
 // // Pin definitions
 // #define ANALOG_PIN 2   // AO - Analog output (ADC1_CH0)
 // #define DIGITAL_PIN 42   // DO - Digital output
@@ -8,6 +11,8 @@
 // int analogValue = 0;
 // int digitalValue = 0;
 // int threshold = 3500;  // Adjust based on your readings (0-4095 for ESP32)
+extern RangedInt WIRE_VOID; // void/open wires
+
 
 void micSetup() {
   Serial.begin(115200);
@@ -21,7 +26,7 @@ void micSetup() {
   delay(1000);
 }
 
-bool micLoop(double timer) {
+bool micLoop(double timer,bool &wire_in) {
 
   Display1.clearDisplay();
   Display1.setTextSize(1.5);
@@ -37,7 +42,37 @@ bool micLoop(double timer) {
   {
     // check for wrong input
     // if wires values change 
+     ADS.readADC(0);
+      int16_t readingA0 = ADS.getValue();
+       ADS.readADC(1);
+      int16_t readingA1 = ADS.getValue();
+       ADS.readADC(2);
+      int16_t readingA2 = ADS.getValue();
+          //First check if wires are in 
+            //No wires are in 
+          if(wire_in == 0)
+          {
+            //Checks if any wires get plugged in
+            if(!WIRE_VOID.isInRange(readingA0) && !WIRE_VOID.isInRange(readingA1) && !WIRE_VOID.isInRange(readingA2))
+            {
+              return false;
+            }
+          }
+          else if(wire_in == 1)
+          {
+            //Checks if any wires were removed
+            if(WIRE_VOID.isInRange(readingA0) && WIRE_VOID.isInRange(readingA1) && WIRE_VOID.isInRange(readingA2))
+            {
+              return false;
+            }
+          }
+
+
     // if key is pressed on keypad
+    char key = keypad.getKey();
+      if(key){
+        return false; // wrong input
+      }
 
     
     // Read analog value (0-4095 on ESP32)
@@ -50,8 +85,8 @@ bool micLoop(double timer) {
     Serial.print("Analog: ");
     Serial.print(analogValue);
     Serial.println();
-    // Serial.print("\t|\tDigital: ");
-    // Serial.print(digitalValue);
+     Serial.print("\t|\tDigital: ");
+     Serial.print(digitalValue);
     
     // Check if sound detected
     // if (digitalValue == HIGH) {
