6bc1a06d4237a455ea507f4f70f17ef25d80b047
Arian-Ari <ATA57@PITT.EDU>
2025-11-19 01:11:16 -0500

    Enhance keypad functionality with wire checks
    Refactor keypad loop to include wire input checks and ADC readings.

--- Defuse_It_Top/combo_keypad.ino
+++ Defuse_It_Top/combo_keypad.ino
@@ -1,6 +1,8 @@
 // #include <Keypad.h>
-#include <Adafruit_SSD1306.h>
 #include <Wire.h>
+#include "ADS1X15.h"
+#include "Voltage_Value.h"
+#include <Adafruit_SSD1306.h>
 
 #define OLED_RESET 4
 //Adafruit_SSD1306 Display1(128,64,&Wire,OLED_RESET);
@@ -21,18 +23,17 @@
 // Keypad keypad = Keypad(makeKeymap(keys), rowPins, colPins, rows, cols);
 
 // String enteredCode = "";
+extern RangedInt WIRE_VOID; // void/open wires
 
 
-
-void setupKeypad() {
+void setupKeypad() 
+{
   Serial.begin(9600);
   Wire.begin();
   Display1.begin(SSD1306_SWITCHCAPVCC, 0x3C);
-
-
 }
 
-bool keypadLoop(double timer) {
+bool keypadLoop(double timer,bool &wire_in) {
   // random keypad code
   String num1 = String(random(0,9));
   String num2 = String(random(0,9));
@@ -62,7 +63,35 @@ bool keypadLoop(double timer) {
     {
       // check for wrong input
       // if wires values change 
-      // if mic goes above threshold
+       ADS.readADC(0);
+      int16_t readingA0 = ADS.getValue();
+       ADS.readADC(1);
+      int16_t readingA1 = ADS.getValue();
+       ADS.readADC(2);
+      int16_t readingA2 = ADS.getValue();
+          //First check if wires are in 
+            //No wires are in 
+          if(wire_in == 0)
+          {
+            //Checks if any wires get plugged in
+            if(!WIRE_VOID.isInRange(readingA0) && !WIRE_VOID.isInRange(readingA1) && !WIRE_VOID.isInRange(readingA2))
+            {
+              return false;
+            }
+          }
+          else if(wire_in == 1)
+          {
+            //Checks if any wires were removed
+            if(WIRE_VOID.isInRange(readingA0) && WIRE_VOID.isInRange(readingA1) && WIRE_VOID.isInRange(readingA2))
+            {
+              return false;
+            }
+          }
+      // if mic is above threshold
+      analogValue = analogRead(ANALOG_PIN);
+      if (analogValue > threshold) {
+        return false; // wrong input
+      }
 
       char key = keypad.getKey();
       if(key)
